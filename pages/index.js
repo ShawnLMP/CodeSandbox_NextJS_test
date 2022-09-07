3dda4c045c17f3f082c63fcac4ebefe782f7b27c
ShawnLMP <shawnleemap@gmail.com>
2022-09-07 11:27:40 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      test: 1123
+    }
+  };
+}
+
+export default function IndexPage(props) {
+  const { test } = props;
+
+  console.log(test);
+
+  return (
+    <div>
+      Hello World. {test}
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+}
